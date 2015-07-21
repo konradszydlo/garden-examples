54ae0fc5a2aa26053c734c039174414bc56a40bf
Konrad Szydlo <konrad_szydlo@yahoo.co.uk>
2015-07-21 19:28:31 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to garden-examples
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/garden_examples/core_test.clj
@@ -0,0 +1,7 @@
+(ns garden-examples.core-test
+  (:require [clojure.test :refer :all]
+            [garden-examples.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
